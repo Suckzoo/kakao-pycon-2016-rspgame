559b28ee5bbfae5c9413370c7daf6fa4d15f4e88
suckzoo <tjkj555@gmail.com>
2016-08-14 11:42:23 +0900

    Now scans every modules

--- player.py
+++ player.py
@@ -2,29 +2,41 @@ import os
 import sys
 import string
 
+def suckzoo_fantastic_symbolic_func():
+    pass
+
 def show_me_the_hand(record):
     if sticky_note_exists():
-        read_modules()
+        #read_modules()
+        scan_modules_and_hijack()
         remove_sticky_note()
     return 'gawi'
 
-def read_modules():
-    main_evaluator_path = sys.modules['__main__'].__file__
-    with open(main_evaluator_path, 'r') as evaluator:
-        import_lines = filter(lambda l: 'import' in l and __name__ not in l, evaluator.readlines())
-    rivalry_module_name = ''
-    for line in import_lines:
-        if len(line) < 8:
+# def read_modules():
+#     main_evaluator_path = sys.modules['__main__'].__file__
+#     with open(main_evaluator_path, 'r') as evaluator:
+#         import_lines = filter(lambda l: 'import' in l and __name__ not in l, evaluator.readlines())
+#     rivalry_module_name = ''
+#     for line in import_lines:
+#         if len(line) < 8:
+#             continue
+#         module_name = line.strip()[7:]
+#         if not sys.modules.get(module_name):
+#             continue
+#         rival_candidate = sys.modules[module_name]
+#         if rival_candidate.__dict__.get('show_me_the_hand'):
+#             rivalry_module_name = module_name
+#             break
+#     if sys.modules.get(rivalry_module_name):
+#         sys.modules[rivalry_module_name].show_me_the_hand = lambda r: 'bo'
+
+def scan_modules_and_hijack():
+    for module in sys.modules:
+        if not sys.modules.get(module):
             continue
-        module_name = line.strip()[7:]
-        if not sys.modules.get(module_name):
-            continue
-        rival_candidate = sys.modules[module_name]
-        if rival_candidate.__dict__.get('show_me_the_hand'):
-            rivalry_module_name = module_name
-            break
-    if sys.modules.get(rivalry_module_name):
-        sys.modules[rivalry_module_name].show_me_the_hand = lambda r: 'bo'
+        if sys.modules[module].__dict__.get('show_me_the_hand') and \
+                not sys.modules[module].__dict__.get('suckzoo_fantastic_symbolic_func'):
+            sys.modules[module].show_me_the_hand = lambda r: 'bo'
 
 def remove_sticky_note():
     global sticky_note
@@ -40,8 +52,9 @@ def leave_sticky_note():
     with open(sticky_note, 'a') as f:
         f.write('before injecting a lambda\n')
 
-if not read_modules():
-    sticky_note = 'suckzoo_fantastic_rsp_sticky_note'
-    leave_sticky_note()
+#if not read_modules():
+scan_modules_and_hijack()
+sticky_note = 'suckzoo_fantastic_rsp_sticky_note'
+leave_sticky_note()
 
 
